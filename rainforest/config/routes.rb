a0ad05e62c881584139abd86016f75032c811fb6
davemerritt <davidmerritt6@gmail.com>
2013-11-11 16:06:54 -0500

    Weiting the review routes, controllers and views

--- rainforest/config/routes.rb
+++ rainforest/config/routes.rb
@@ -1,4 +1,7 @@
 Rainforest::Application.routes.draw do
+  get "reviews/show"
+  get "reviews/new"
+  get "reviews/edit"
   get "sessions/new"
   get "sessions/create"
   get "sessions/destroy"
@@ -8,6 +11,9 @@ Rainforest::Application.routes.draw do
   resources :products
   resources :users, :only => [:new, :create]
   resources :sessions, :only => [:new, :create, :destroy]
+  resources :products do
+    resources :reviews, :except => [:index]
+  end
 
   root :to => "products#index"
 
