cb2c45a000460fa9ccbb843afff09e852d4fb372
davemerritt <davidmerritt6@gmail.com>
2013-11-11 13:54:45 -0500

    Creating a user model, controller and view

--- rainforest/app/models/product.rb
+++ rainforest/app/models/product.rb
@@ -3,9 +3,9 @@ class Product < ActiveRecord::Base
 	validates :price_in_cents, :numericality => {:only_integer => true}
 
 	
-	def formatted_price
-		price_in_dollars = price_in_cents.to_f / 100
-		sprintf("%.2f, price_in_dollars")
-	end
+	  def formatted_price
+	    price_in_dollars = price_in_cents.to_f / 100
+	    sprintf("%.2f", price_in_dollars)
+	  end
 
 end
